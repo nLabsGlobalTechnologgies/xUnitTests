c5a81cf5da51db4a59b8e36648682dff9789a8bd
Cuma KÖSE <turkmvc@gmail.com>
2024-04-30 23:50:40 +0300

    New Update

--- 2.RealWorld/src/RealWorld.WebAPI/Repositories/UserRepository.cs
+++ 2.RealWorld/src/RealWorld.WebAPI/Repositories/UserRepository.cs
@@ -4,50 +4,42 @@ using RealWorld.WebAPI.Models;
 
 namespace RealWorld.WebAPI.Repositories;
 
-public class UserRepository : IUserRepository
+public class UserRepository(AppDbContext context) : IUserRepository
 {
-    private readonly AppDbContext _context;
-
-    public UserRepository(AppDbContext context)
-    {
-        _context = context;
-    }
 
     public async Task<bool> CreateAsync(User user, CancellationToken cancellationToken = default)
     {
-        await _context.Users.AddAsync(user, cancellationToken);
-        var result = _context.SaveChanges();
+        await context.Users.AddAsync(user, cancellationToken);
+        var result = context.SaveChanges();
         return result > 0;
     }
 
     public async Task<bool> DeleteAsync(User user, CancellationToken cancellationToken = default)
     {
-        _context.Users.Remove(user);
-        var result = await _context.SaveChangesAsync(cancellationToken);
+        context.Users.Remove(user);
+        var result = await context.SaveChangesAsync(cancellationToken);
         return result > 0;
     }
 
     public async Task<List<User>> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        var result = await _context.Users.ToListAsync(cancellationToken);
-        return result;
+        return await context.Users.ToListAsync(cancellationToken);
     }
 
     public async Task<User?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
-        var user = await _context.Users.FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
-        return user;
+        return await context.Users.FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
     }
 
     public async Task<bool> NameIsExists(string name, CancellationToken cancellationToken = default)
     {
-        return await _context.Users.AnyAsync(p => p.Name == name, cancellationToken);
+        return await context.Users.AnyAsync(p => p.Name == name, cancellationToken);
     }
 
     public async Task<bool> UpdateAsync(User user, CancellationToken cancellationToken = default)
     {
-        _context.Update(user);
-        var result = await _context.SaveChangesAsync(cancellationToken);
+        context.Update(user);
+        var result = await context.SaveChangesAsync(cancellationToken);
         return result > 0;
     }
 }
